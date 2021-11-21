319c8d35770ef1256205cd5ec377b7518c655d4d
3119005408陈泽同_local <834663474@qq.com>
2021-11-21 15:00:45 +0800

    更新Plan类属性

--- app/src/main/java/com/wqf/clock/Plan.java
+++ app/src/main/java/com/wqf/clock/Plan.java
@@ -1,4 +1,30 @@
 package com.wqf.clock;
 
 public class Plan {
+    String name;   // 计划的名称
+    String description;  // 计划的描述
+    long workTime;  // 单次学习的时间
+    long breakTime;  // 单次休息的时间
+    long beginTime;  // 开始时间
+    long finishTime;  // 结束时间
+    Mould mould;  // 使用的闹铃模板
+    ArrayList<Clock> clocks;  // 一组闹钟
+
+    Plan() {
+        name = "新计划";
+        description = "";
+        workTime = 0;
+        breakTime = 0;
+        beginTime = System.currentTimeMillis();
+        finishTime = beginTime;
+        mould = new Mould();
+        clocks = this.setClocks();
+    }
+
+    Clock[] setClocks() {
+        // 根据开始时间、结束时间、单次学习时间和单次休息时间设定一定数量的闹钟
+
+        Clock[] clks =
+    }
+
 }
