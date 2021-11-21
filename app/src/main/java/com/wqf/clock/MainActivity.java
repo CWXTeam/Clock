89ff0ba689a0a09635504c7d5d056a4eba3e1698
wqf <1724840879@qq.com>
2021-11-21 13:21:52 +0800

    增加了MyFileUtils.java，现在可以调用此工具类获得所有配置文件的名称了

--- app/src/main/java/com/wqf/clock/MainActivity.java
+++ app/src/main/java/com/wqf/clock/MainActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView diana;
-
+    TextView textView;
+    static String path="/data/data/com.wqf.clock/shared_prefs";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.diana_said);
+        SpUtils sp=SpUtils.getInstance("user");
+        sp.save("name","xiaoming");
+        sp.save("age",24);
+        String str=sp.getString("name","");
+        textView.setText(str);
+        List<String> arrayList=MyFileUtils.getFilesAllName(path);
+        textView.setText(arrayList.get(0));
     }
 }
