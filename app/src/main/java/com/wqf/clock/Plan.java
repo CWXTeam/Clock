f469e93ca634cc2703e16739fb201a9d24b096eb
3119005408陈泽同_local <834663474@qq.com>
2021-11-21 16:16:14 +0800

    完成Plan类

--- app/src/main/java/com/wqf/clock/Plan.java
+++ app/src/main/java/com/wqf/clock/Plan.java
@@ -1,16 +1,16 @@
 package com.wqf.clock;
 
 public class Plan {
-    String name;   // 计划的名称
-    String description;  // 计划的描述
-    long workTime;  // 单次学习的时间
-    long breakTime;  // 单次休息的时间
-    long beginTime;  // 开始时间
-    long finishTime;  // 结束时间
-    Mould mould;  // 使用的闹铃模板
-    ArrayList<Clock> clocks;  // 一组闹钟
+    protected String name;   // 计划的名称
+    protected String description;  // 计划的描述
+    protected long workTime;  // 单次学习的时间
+    protected long breakTime;  // 单次休息的时间
+    protected long beginTime;  // 开始时间
+    protected long finishTime;  // 结束时间
+    protected Mould mould;  // 使用的闹铃模板
+    protected ArrayList<Clock> clocks;  // 一组闹钟
 
-    Plan() {
+    protected Plan() {
         name = "新计划";
         description = "";
         workTime = 0;
@@ -18,13 +18,55 @@ public class Plan {
         beginTime = System.currentTimeMillis();
         finishTime = beginTime;
         mould = new Mould();
-        clocks = this.setClocks();
+        clocks = new ArrayList<Clock>();
     }
 
-    Clock[] setClocks() {
-        // 根据开始时间、结束时间、单次学习时间和单次休息时间设定一定数量的闹钟
+    protected ArrayList<Clock> setClocks() throws ClockException{
+        // 根据开始时间、结束时间、单次学习时间和单次休息、使用的闹铃模板时间设定一定数量的闹钟
 
-        Clock[] clks =
+        if(workTime <= 0){
+            throw new ClockException("工作一次的时间不能为0！");
+        }
+        else if(breakTime <= 0){
+            throw new ClockException("休息一次的时间不能为0！");
+        }
+        else if(finishTime <= beginTime){
+            throw new ClockException("结束时间不能和开始时间相等！")
+        }
+        else if(workTime > (finishTime - beginTime)){
+            throw new ClockException("工作一次的时间不能超过总时间！")
+        }
+        else if(breakTime > (finishTime - beginTime)){
+            throw  new ClockException("休息一次的时间不能超过总时间！")
+        }
+
+        ArrayList<Clock> clks = new ArrayList<clock>();
+        long clkTime = beginTime;
+
+        clks.add(new Clock(clkTime, new Mould(), "WORK"));  // 工作时间开始！
+
+        while(true){
+            // 如果工作一段时间后还没到finishTime，则响一次下课铃，开始休息
+            clkTime += workTime;
+            if(clkTime < finishTime){
+                clks.add(new Clock(clkTime, new Mould(), "BREAK"));
+            }
+            else{  // 否则，在到达finishTime时响一次下课铃，结束
+                clks.add(new Clock(finishTime, new Mould(), "BREAK"));
+                break;
+            }
+
+            // 如果休息一段时间后还没到finishTime，则响一次上课铃，开始工作
+            clkTime += breakTime;
+            if(clkTime < finishTime){
+                clks.add(clkTime, new Mould(), "WORK");
+            }
+            else{  // 否则，到达finishTime时再响一次下课铃，结束
+                clks.add(new Clock(finishTime, new Mould(), "BREAK"));
+                break;
+            }
+        }
+
     }
 
 }
