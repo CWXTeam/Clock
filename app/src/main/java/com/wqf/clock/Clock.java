2b58cefc586c5a2c5769164773cbeb97570060fc
wqf <1724840879@qq.com>
2021-12-02 13:01:02 +0800

    简单地实现了clock类中的startWork方法

--- app/src/main/java/com/wqf/clock/Clock.java
+++ app/src/main/java/com/wqf/clock/Clock.java
@@ -1,6 +1,12 @@
 package com.wqf.clock;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
 public class Clock {
+    AlarmManager alarmManager;
     long ringTime;  // 响铃时间
     Mould mould;  // 闹钟模板
     String mode;  // 上课铃="WORK"；下课铃="BREAK"
@@ -32,4 +38,9 @@ public class Clock {
         return;
     }
 
+    protected void startWork(Context context){
+        Intent intent = new Intent(context, ClockActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, ringTime, pi);
+    }
 }
